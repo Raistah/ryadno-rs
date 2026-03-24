5e0f1200814f11260742cba48a1a95a32b7875e8
raistah <bigkuraz@gmail.com>
2026-03-24 19:30:37 +0200

    feat: finish DataPath, add tests

--- ryadno/src/structs/data_path.rs
+++ ryadno/src/structs/data_path.rs
@@ -29,22 +29,22 @@ impl DataPath {
 
     pub fn absolute(mut self) -> Self {
         self.is_absolute = true;
-        self
+        self.normalize()
     }
 
     pub fn relative(mut self) -> Self {
         self.is_absolute = false;
-        self
+        self.normalize()
     }
 
     pub fn push(mut self, segment: Segment) -> Self {
         self.segments.push(segment);
-        self
+        self.normalize()
     }
 
     pub fn extend(mut self, end: DataPath) -> Self {
         self.segments.extend(end.segments);
-        self
+        self.normalize()
     }
 
     pub fn normalize(mut self) -> Self {
@@ -77,12 +77,39 @@ impl DataPath {
         self
     }
 
-    // It is probably imposible to find value if path is relative.
-    // So i need to return Option
-    // I need to have recursive function
-    // I need to decide how to handle StepBack
-    pub fn find_value(&self, value: &Value) -> Option<&Value> {
-        todo!()
+    // StepBack is always skiped, because path should be normolized at this point
+    pub fn find_value<'a>(&self, value: &'a Value) -> Option<&'a Value> {
+        let mut found_value = Some(value);
+        for segment in self.segments.iter() {
+            match found_value {
+                Some(v) => match v {
+                    Value::Array(v) => match segment {
+                        Segment::StepBack => {}
+                        Segment::Index(i) => match v.get(i.to_owned()) {
+                            Some(v) => {
+                                found_value = Some(v);
+                            }
+                            None => return None,
+                        },
+                        Segment::Key(_) => return None,
+                    },
+                    Value::Object(v) => match segment {
+                        Segment::StepBack => {}
+                        Segment::Index(_) => return None,
+                        Segment::Key(k) => match v.get(k) {
+                            Some(v) => {
+                                found_value = Some(v);
+                            }
+                            None => return None,
+                        },
+                    },
+                    _ => return None,
+                },
+                None => return None,
+            }
+        }
+
+        found_value
     }
 }
 
@@ -111,6 +138,7 @@ impl From<&str> for DataPath {
                 .collect(),
             is_absolute: path.starts_with("/"),
         }
+        .normalize()
     }
 }
 
@@ -139,6 +167,7 @@ impl From<String> for DataPath {
                 .collect(),
             is_absolute: path.starts_with("/"),
         }
+        .normalize()
     }
 }
 
@@ -165,23 +194,86 @@ impl Display for DataPath {
 
 #[cfg(test)]
 mod test {
+    use serde_json::{Value, json};
+
     use crate::structs::data_path::DataPath;
 
     #[test]
     fn test_data_path_normalization() {
-        assert_eq!(
-            DataPath::from("a/b/c/../../d").normalize(),
-            DataPath::from("a/d")
-        );
+        assert_eq!(DataPath::from("a/b/c/../../d"), DataPath::from("a/d"));
+
+        assert_eq!(DataPath::from("../a/b/c/../../d"), DataPath::from("../a/d"));
 
         assert_eq!(
-            DataPath::from("../a/b/c/../../d").normalize(),
-            DataPath::from("../a/d")
-        );
-
-        assert_eq!(
-            DataPath::from("../a/b/c/../../d").absolute().normalize(),
+            DataPath::from("../a/b/c/../../d").absolute(),
             DataPath::from("/a/d")
+        );
+    }
+
+    #[test]
+    fn test_data_finding() {
+        let value: Value = json!({
+            "a": "value a",
+            "b": {
+                "vec": [
+                    "vec 1",
+                    "vec 2",
+                    "vec 3",
+                    "vec 4",
+                    "vec 5",
+                ],
+            },
+            "c": [
+                {
+                    "subVec1": [],
+                    "subVec2": [],
+                    "subVec3": [
+                        "subVec3 1",
+                        "subVec3 2",
+                        "subVec3 3",
+                        "subVec3 4",
+                    ],
+                },
+            ]
+        });
+
+        let path1 = DataPath::from("a");
+        assert_eq!(
+            path1.find_value(&value),
+            Some(&Value::String("value a".to_string()))
+        );
+
+        let path2 = DataPath::from("/a/d");
+        assert_eq!(path2.find_value(&value), None);
+
+        let path3 = DataPath::from("/b/vec/[3]");
+        assert_eq!(
+            path3.find_value(&value),
+            Some(&Value::String("vec 4".to_string()))
+        );
+
+        let path4 = DataPath::from("/c/[0]/subVec3/[2]");
+        assert_eq!(
+            path4.find_value(&value),
+            Some(&Value::String("subVec3 3".to_string()))
+        );
+
+        let path5 = path4.extend(DataPath::from("../../../../b/vec/[0]"));
+        assert_eq!(
+            path5.find_value(&value),
+            Some(&Value::String("vec 1".to_string()))
+        );
+
+        let path6 = DataPath::from("/b/vec");
+        assert_eq!(
+            path6.find_value(&value),
+            Some(&Value::Array(vec![
+                Value::String("vec 1".to_string()),
+                Value::String("vec 2".to_string()),
+                Value::String("vec 3".to_string()),
+                Value::String("vec 4".to_string()),
+                Value::String("vec 5".to_string()),
+            ]))
         );
     }
 }
